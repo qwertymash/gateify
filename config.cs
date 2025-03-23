a61d0cf20c1657e659f6021060e104db2767dffe
qwertymash <egreabu@gmail.com>
2025-03-23 17:46:23 +0200

    Add project files.

---
+++ config.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+using Exiled.API.Interfaces;
+
+namespace aegis.gateify
+{
+    public class Config : IConfig
+    {
+        [Description("Whether the plugin is enabled")]
+        public bool IsEnabled { get; set; }
+        [Description("Whether debug messages should be shown in the console")]
+        public bool Debug { get; set; }
+    }
+}
---
+++ plugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Doors;
+using Exiled.Events.EventArgs.Server;
+using Exiled.Events.Features;
+using InventorySystem.Items.Thirdperson.LayerProcessors;
+
+namespace aegis.gateify
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Gateify";
+        public override string Author => "aegis";
+        public override Version Version => new Version(0,1,0);
+        private Door GateA = null;
+        private Door GateB = null;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted += this.GetGates;
+            Exiled.Events.Handlers.Server.RespawnedTeam += this.Respawn;
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= this.GetGates;
+            Exiled.Events.Handlers.Server.RespawnedTeam -= this.Respawn;
+        }
+
+        private void GetGates()
+        {
+            GateA = Door.Get("GATE_A");
+            GateB = Door.Get("GATE_B");
+            LockGate(GateA);
+            LockGate(GateB);
+        }
+
+        private void Respawn(RespawnedTeamEventArgs team)
+        {
+            Log.Debug($"{team.Wave} arrived, unlocking gate");
+        }
+
+        private void LockGate(Door gate)
+        {
+            if (gate != null)
+            {
+                gate.Lock(long.MaxValue, DoorLockType.AdminCommand);
+            }
+            else
+            {
+                Log.Error("Gate is null (????)");
+            }
+        }
+
+        private void UnlockGate(Door gate)
+        {
+            if (gate != null)
+            {
+                gate.Unlock(long.MaxValue, DoorLockType.AdminCommand);
+            }
+            else
+            {
+                Log.Error("Gate is null (????)");
+            }
+        }
+    }
+}
